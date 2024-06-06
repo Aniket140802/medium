bcc07a980290492301110585e0df489d338073fd
Aniket140208 <mahajananiket51@gmail.com>
2024-06-06 13:43:32 +0530

    Added some more things

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import {  sign, verify } from 'hono/jwt'
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
+import { cors } from 'hono/cors'
 
 const app = new Hono()
 
